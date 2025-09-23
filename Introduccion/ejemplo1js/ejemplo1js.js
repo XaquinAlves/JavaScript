18af3e0d049c50510ab0b371c5fdf54ce6060665
XaquinAlves <xaquinalves@gmail.com>
2025-09-23 13:22:52 +0200

    Learning JavaScript

--- Introduccion/ejemplo1js/ejemplo1js.js
+++ Introduccion/ejemplo1js/ejemplo1js.js
@@ -1,6 +1,22 @@
 console.info(document);
 document.getElementById("prueba").innerHTML = "Aquí si funciona";
 function cambiarTexto() {
-    console.warn(document);
+    console.warn("Cambiando texto");
     document.getElementById("prueba").innerHTML = "Otro texto";
+}
+
+function cambiaTamano() {
+    console.warn("Cambia tamaño");
+    document.getElementById("p1").style.fontSize = "10px";
+}
+
+function cambiaImagen() {
+    var imagen = document.getElementById("myImg");
+    if (imagen.src.match("green")) {
+        imagen.src =
+            "http://myfpschool.com/wp-content/uploads/2016/06/myblack.jpeg";
+    } else {
+        imagen.src =
+            "http://myfpschool.com/wp-content/uploads/2016/06/mygreen.jpeg";
+    }
 }
