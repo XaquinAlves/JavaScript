a81f60930599930b35197db612813e5597a79597
XaquinAlves <xaquinalves@gmail.com>
2025-09-30 13:02:16 +0200

    Calculadora HTML + JS

--- Introduccion/calculadora_basica/calculadorabasica.js
+++ Introduccion/calculadora_basica/calculadorabasica.js
@@ -1,45 +1,73 @@
 // Acumulador
-var num1 = 0;
-var num2 = 0;
-var operador = null;
+var num1 = '0';
+var num2 = '0';
+var operador = undefined;
 
 function darC() {
-    num1 = 0;
-    num2 = 0;
-    document.getElementById("valor_numero").value = 0;
+    num1 = '0';
+    num2 = '0';
+    operador = undefined;
+    resfrescar();
+    console.log("borrado");
 }
 
 function darNumero(numero) {
-        if (num1 == 0) {
-            num1 = numero
+    if (num1 == '0') {
+        num1 = numero;
+    } else if (operador == undefined) {
+        num1 += numero;
+    } else {
+        if (num2 == '0') {
+            num2 = numero;
         } else {
-            num1 += numero;
+            num2 += numero;
         }
-        document.getElementById("valor_numero").value = num1;
+    }
+    resfrescar();
+    console.log("Numero añadido.Num1: "+num1+" Num2: "+num2);
 }
 
 function darComa() {
-
+    if (operador == undefined && num1.indexOf('.') == -1){
+        num1 += '.';
+        console.log("Coma añadida a num1: "+ num1);
+    } else if (num2.indexOf('.') == -1) {
+        num2 += '.';
+        console.log("Coma añadida a num2" + num2);
+    }
+    resfrescar();
 }
 
 function operar(valor) {
-    switch(valor) {
-        case 1: operador = '+';
-        break;
-        case 2: operador = '-';
-        break;
-        case 3: operador = 'x';
-        break;
-        case 4: operador = '/';
-        break;
-        case 5: operador = '^'
-    }
+    operador = valor;
+    resfrescar();
+    console.log("Operador añadido: " + operador);
 }
 
 function esIgual() {
-
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
+    switch(operador) {
+        case '+': num1 += num2;
+            break;
+        case '-': num1 -= num2;
+            break;
+        case 'x': num1 *= num2;
+            break;
+        case '/': num1 /= num2;
+            break;
+        case '^': num1 **= num2;
+    }
+    num2 = '0';
+    operador = undefined;
+    resfrescar();
+    console.log("Operación realizada")
 }
 
 function resfrescar() {
-
+    if (num1 == 0 || operador == undefined) {
+        document.getElementById("valor_numero").value = num1;
+    } else {
+        document.getElementById("valor_numero").value = num1 + operador + num2;
+    }
 }
