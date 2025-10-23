88938e5faca0566919102e4d241196a06061cc92
XaquinAlves <xaquinalves@gmail.com>
2025-10-23 14:26:45 +0200

    rot13 js INCOMPLETO

---
+++ Proyectos_FreeCodeCamp/Legacy_JavaScript_ADS/cifradoROT13/rot13.js
@@ -0,0 +1,9 @@
+function rot13(texto) {
+    let refDerecho = Array.from('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+    let refReves = Array.from('NOPQRSTUVWXYZABCDEFGHIJKLM')
+    let arrayText = Array.from(texto);
+
+    for(let i = 0; i < arrayText.length; i++){
+        
+    }
+}
