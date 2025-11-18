3147f083090c169a234e4f2561a0e881d7ab1907
XaquinAlves <xaquinalves@gmail.com>
2025-11-18 13:43:06 +0100

    Done test tel

--- Sede_electronica_ministerio/scripts.js
+++ Sede_electronica_ministerio/scripts.js
@@ -1,7 +1,19 @@
-let form = document.getElementById("formUsuario");
+const form = document.getElementById("formUsuario");
+const telefonoInput = document.getElementById("input_telef");
+
+// Expresión regular para validar el formato del teléfono móvil español
+const telefonoPattern = /^(?:\+34|0034|34)?[ -]*(6|7)[ -]*([0-9][ -]*){8}$/;
 
 form.addEventListener("submit", function(event) {
     event.preventDefault();
+
+    if (!telefonoPattern.test(telefonoInput.value)) {
+        document.getElementById("errorTel").style = "visibility: visible";
+        return;
+    } else {
+        document.getElementById("errorTel").style = "visibility: hidden";
+    }
+
     if (document.getElementById("consentimiento").classList.contains("is-visible")) {
         document.getElementById("consentimiento").focus();
     } else {
