c4e13821d569a2488009a9bf9d7f9af7dab0d706
XaquinAlves <xaquinalves@gmail.com>
2025-11-20 14:12:04 +0100

    CustomValidity

--- Sede_electronica_ministerio/scripts.js
+++ Sede_electronica_ministerio/scripts.js
@@ -1,49 +1,8 @@
 const form = document.getElementById("formUsuario");
 
 function validar() {
-    let check = [];
-
     validarNombre(document.getElementById("input_nombre"));
     validarPrimerApellido(document.getElementById("input_apellido1"));
-
-    //Comprobamos la fecha de nacimiento
-    validarFechaNacimiento(document.getElementById("input_fecnac"));
-    //Comprobamos el tipo de documento
-    check.push(
-        validarDocumento(
-            document.getElementById("tipo_documento"),
-            document.getElementById("input_documento")
-        )
-    );
-    //Comprobamos las contraseñas
-    check.push(
-        validarPass(
-            document.getElementById("input_pass").value,
-            document.getElementById("input_pass_repeat").value
-        )
-    );
-    //Comprobamos los correos
-    check.push(
-        validarCorreo(
-            document.getElementById("input_correo").value,
-            document.getElementById("input_correo_repeat").value
-        )
-    );
-    //Comprobamos el telefono
-    check.push(validarTelef(document.getElementById("input_telef").value));
-
-    //Si pasa todas las comprobaciones, mostramos el consentimiento
-    if (!check.includes(false)) {
-        document.getElementById("consentimiento").classList.add("is-visible");
-    }
-}
-
-//Event listener para checkear el formulario
-form.addEventListener("submit", function (event) {
-    event.preventDefault(); //Detiene el envío del formulario
-    let check = [];
-
-    validarNombre(document.getElementById("input_nombre"));
 
     //Comprobamos la fecha de nacimiento
     validarFechaNacimiento(document.getElementById("input_fecnac"));
@@ -53,26 +12,26 @@ form.addEventListener("submit", function (event) {
         document.getElementById("input_documento")
     );
 
+    validarNumSoporte(document.getElementById("input_numsoporte"));
     //Comprobamos las contraseñas
     validarPass(
         document.getElementById("input_pass"),
         document.getElementById("input_pass_repeat")
     );
     //Comprobamos los correos
-    check.push(
-        validarCorreo(
-            document.getElementById("input_correo").value,
-            document.getElementById("input_correo_repeat").value
-        )
+
+    validarCorreo(
+        document.getElementById("input_correo"),
+        document.getElementById("input_correo_repeat")
     );
     //Comprobamos el telefono
-    check.push(validarTelef(document.getElementById("input_telef").value));
+    validarTelef(document.getElementById("input_telef"));
 
     //Si pasa todas las comprobaciones, mostramos el consentimiento
     if (form.checkValidity()) {
         document.getElementById("consentimiento").classList.add("is-visible");
     }
-});
+}
 
 //Para el boton de aceptar consentimiento
 function aceptarConsentimiento() {
@@ -105,6 +64,7 @@ function validarFechaNacimiento(fecha) {
     const anoMilisegundos = 31557600000;
     //Calculamos la edad en años con la fecha de nacimiento dada
     let edad = Math.abs(new Date() - new Date(fecha.value)) / anoMilisegundos;
+
     if (edad < 18) {
         fecha.setCustomValidity("Debe ser mayor de edad");
     } else {
@@ -117,7 +77,9 @@ function validarDocumento(tipoDocumento, numDocumento) {
     const dniPattern = /^\d{8}[A-HJ-NP-TV-Z]$/i;
     const niePattern = /^(X|Y|Z)\d{7}[A-HJ-NP-TV-Z]$/;
 
-    if (tipoDocumento.value == "DNI") {
+    if (numDocumento.validity.valueMissing) {
+        numDocumento.setCustomValidity("El Nº de documento es obligatorio");
+    } else if (tipoDocumento.value == "DNI") {
         //Si es un DNI
         //Comprobamos el DNI
         if (!dniPattern.test(numDocumento.value)) {
@@ -147,51 +109,63 @@ function validarDocumento(tipoDocumento, numDocumento) {
             document.getElementById("errorDni").style = "visibility: hidden";
             numDocumento.setCustomValidity("");
         }
+    } else {
+        numDocumento.setCustomValidity("");
+    }
+}
+
+function validarNumSoporte(numSoporte) {
+    if (numSoporte.validity.valueMissing) {
+        numSoporte.setCustomValidity("El Nº de soporte es obligatorio");
+    } else {
+        numSoporte.setCustomValidity("");
     }
 }
 
 function validarPass(pass, repeat) {
     const passPattern = /[!@#%\^&*]{2,}/;
 
-    if (!passPattern.test(pass.value)) {
+    if (pass.validity.valueMissing) {
+        pass.setCustomValidity("La contraseña es obligatoria");
+    } else if (pass.validity.tooShort) {
+        pass.setCustomValidity(
+            "La contraseña debe tener al menos 12 caracteres"
+        );
+    } else if (!passPattern.test(pass.value)) {
         //Si no contiene al menos 2 caracteres especiales, mostramos el error
         document.getElementById("errorPass").innerHTML =
-            "Las contraseña debe contener al menos 2 caracteres especiales (!@#%^&*).";
+            "La contraseña debe contener al menos 2 caracteres especiales (!@#%^&*).";
         document.getElementById("errorPass").style = "visibility: visible";
-        pass.setCustomValidity("Contraseña poco segura")
+        pass.setCustomValidity("Contraseña poco segura");
     } else if (pass.value !== repeat.value) {
         //Si no coinciden, mostramos el error
         document.getElementById("errorPass").innerHTML = "";
         document.getElementById("errorPass").style = "visibility: hidden";
-        repeat.setCustomValidity("Las contraseñas no coinciden")
+        repeat.setCustomValidity("Las contraseñas no coinciden");
     } else {
         //Si coinciden
         document.getElementById("errorPass").innerHTML = "";
         document.getElementById("errorPass").style = "visibility: hidden";
-        pass.setCustomValidity("")
-        repeat.setCustomValidity("")
+        pass.setCustomValidity("");
+        repeat.setCustomValidity("");
     }
 }
 
 function validarCorreo(correo, repeat) {
     const emailPattern = /^[a-z]+\w*@[a-z0-9]+\.[a-z]+(\.[a-z]*)*$/iu;
 
-    if (!emailPattern.test(correo)) {
+    if (correo.validity.valueMissing) {
+        correo.setCustomValidity("El correo es obligatorio")
+    }
+    if (!emailPattern.test(correo.value)) {
         //Si no cumple el patrón, mostramos el error
-        document.getElementById("errorEmail").innerHTML =
-            "El correo tiene formato erróneo.";
-        document.getElementById("errorEmail").style = "visibility: visible";
-        return false;
-    } else if (correo !== repeat) {
+        correo.setCustomValidity("El correo tiene un formato erróneo.")
+    } else if (correo.value !== repeat.value) {
         //Si no coinciden, mostramos el error
-        document.getElementById("errorEmail").innerHTML =
-            "Los correos no coinciden.";
-        document.getElementById("errorEmail").style = "visibility: visible";
-        return false;
+        repeat.setCustomValidity("Los correos no coinciden.");
     } else {
-        document.getElementById("errorEmail").innerHTML = "";
-        document.getElementById("errorEmail").style = "visibility: hidden";
-        return true;
+        correo.setCustomValidity("");
+        repeat.setCustomValidity("");
     }
 }
 
@@ -199,16 +173,13 @@ function validarTelef(telef) {
     const telefonoPattern =
         /^(\+34|0034|34)?[ -]*(6|7|8|9)[ -]*([0-9][ -]*){8}$/;
 
-    if (!telefonoPattern.test(telef)) {
+    if (telef.validation.valueMissing) {
+        telef.setCustomValidity("El teléfono es obligatorio");
+    } else if (!telefonoPattern.test(telef.value)) {
         //Si no cumple el patron, mostramos el error
-        document.getElementById("errorTel").innerHTML =
-            "Formato de teléfono no válido.";
-        document.getElementById("errorTel").style = "visibility: visible";
-        return false;
+        telef.setCustomValidity("Formato de teléfono inválido");
     } else {
         //Si lo cumple
-        document.getElementById("errorTel").innerHTML = "";
-        document.getElementById("errorTel").style = "visibility: hidden";
-        return true;
+        telef.setCustomValidity("El teléfono es obligatorio");
     }
 }
